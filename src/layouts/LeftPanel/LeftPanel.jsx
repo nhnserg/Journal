be149de4442f7c2215d4386f5b8e7fe1bc72be59
Serhii <124461383+nhnserg@users.noreply.github.com>
2023-10-30 20:43:32 +0100

    1

--- src/layouts/LeftPanel/LeftPanel.jsx
+++ src/layouts/LeftPanel/LeftPanel.jsx
@@ -1,11 +1,7 @@
-import './LeftPanel.css';
+import "./Leftpanel.css";
 
-function LeftPanel({ children }) {
-	return (
-		<div className='left-panel'>
-			{children}
-		</div>
-	);
+function Leftpanel({ children }) {
+  return <div className="left-panel">{children}</div>;
 }
 
-export default LeftPanel;
+export default Leftpanel;
