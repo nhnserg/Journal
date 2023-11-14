39e71bfb4ab9405d9855b6f480fee8dba3fd7f55
Serhii <124461383+nhnserg@users.noreply.github.com>
2023-11-14 19:26:29 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,33 +9,32 @@ import { useLocalStorage } from './hooks/use-localstorage.hook';
 import { UserContextProvidev } from './context/user.context';
 import { useState } from 'react';
 
-
+function mapItems(items) {
+	if (!items) {
+		return [];
+	}
+	return items.map(i => ({
+		...i,
+		date: new Date(i.date)
+	}));
+}
 
 function App() {
-	function mapItems(items) {
-		if (!items) {
-			return [];
-		}
-		return items.map(i => ({
-			...i,
-			date: new Date(i.date)
-		}));
-	}
-
 	const [items, setItems] = useLocalStorage('data');
 	const [selectedItem, setSelectedItem] = useState(null);
 	console.log('App');
 
 	const addItem = item => {
-		if (item.id) {
+		if (!item.id) {
 			setItems([...mapItems(items), {
 				...item,
 				date: new Date(item.date),
-				id: items.length > 0 ? Math.max(...items.map(i => i.id)) + 1 : 1
+				id: items?.length > 0 ? Math.max(...items.map(i => i.id)) + 1 : 1
 			}]);
 		} else {
 			setItems([...mapItems(items).map(i => {
 				if (i.id === item.id) {
+
 					return {
 						...item
 					};
