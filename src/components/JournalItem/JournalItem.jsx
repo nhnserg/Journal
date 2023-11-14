8f14815d9be007420490cd0056804a963feb10f5
Serhii <124461383+nhnserg@users.noreply.github.com>
2023-11-14 18:58:09 +0100

    Update JournalItem.jsx

--- src/components/JournalItem/JournalItem.jsx
+++ src/components/JournalItem/JournalItem.jsx
@@ -1,16 +1,16 @@
 import './JournalItem.css';
 
 function JournalItem({ title, post, date }) {
-	const formatedDate = new Intl.DateTimeFormat('ru-RU').format(date);
+	const formattedDate = new Intl.DateTimeFormat('ru-RU').format(date);
 
 	return (
-		<>
-			<h2 className="journal-item__header">{title}</h2>
-			<h2 className="journal-item__body">
-				<div className="journal-item__date">{formatedDate}</div>
-				<div className="journal-item__text">{post}</div>
-			</h2>
-		</>
+		<div className="journal-item">
+			<div className="journal-item__header">{title}</div>
+			<div className="journal-item__body">
+				<div className="journal-item__date">{formattedDate}</div>
+				<p className="journal-item__text">{post}</p>
+			</div>
+		</div>
 	);
 }
 
