b1cdc6aa103e302e514ee33d669ea621fe77604d
Serhii <124461383+nhnserg@users.noreply.github.com>
2023-11-14 19:06:11 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
 			setItems([...mapItems(items), {
 				...item,
 				date: new Date(item.date),
-				id: items > 0 ? Math.max(...items.map(i => i.id)) + 1 : 1
+				id: items.length > 0 ? Math.max(...items.map(i => i.id)) + 1 : 1
 			}]);
 		} else {
 			setItems([...mapItems(items).map(i => {
